ff52d079f40b57282cb5f317f4865ec930d260f0
jpileto <jp.ileto@icloud.com>
2016-08-12 22:30:51 -0400

    Add attributes & change instance variables

--- sales-taxes.rb
+++ sales-taxes.rb
@@ -6,9 +6,17 @@
 
 
 class Receipt
-  def initialize(report)
-    @report = report
+
+  attr_reader :list_of_item, :category
+
+  def initialize(list_of_item, category, sales_tax, tot_price)
+    @list_of_item = list_of_item
+    @category = category
+    @sales_tax = sales_tax
+    @tot_price = tot_price
   end
+
+
 end
 # def initialize(product, price, tax_status)
 #   @product = product
@@ -24,29 +32,61 @@ end
 #   end
 # end
 
-
-
-class Item < Receipt
+class Item
   def initialize(name, category, imported, price)
     @name = name
     @category = category
-    @imported = true
+    @imported = imported
     @price = price
   end
+# shelf price, total price, tax
+  # def category
+  #   if other
+  #     @price * 1.10
+  #   else
+  #     @price
+  #   end
+  # end
+  #
+  # def imported
+  #   if imported == true
+  #     @price * 1.05
+  #   else
+  #     @price
+  #   end
+  # end
 
-  def category
-    if other
-      price * 1.10
+  def tax
+    if category == false
+      t = @price * 0.10
     else
-      price
+      @price
     end
+  end
 
-    def imported
-      if imported == true
-        price * 1.05
-      end
+  def duty
+    if imported == true
+      d = @price * 0.15
+    else
+      @price
+    end
+  end
+
+  # or is it better to use case-when??
+  case name
+  when condition
+
+  end
+
+
+
+
+
+
+
+
 end
 
-# b = Item.new("book", "exempt", false, 12.49)
+b = Item.new("book", "exempt", false, 12.49)
 # m = Item.new("music CD", "other",  false, 14.99)
 # c = Item.new("chocolate bar", "other", false, 0.85)
